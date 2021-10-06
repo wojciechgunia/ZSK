424573be064628c8185ae646884fa82412402556
Wojciech Gunia <81011035+wojciechgunia@users.noreply.github.com>
2021-10-06 08:03:46 +0200

    Add files via upload

---
+++ zsk/PP/4.2_formularze.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang=pl dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Figury geometryczne</title>
+  </head>
+  <body>
+    <h3>Figury geometryczne</h3>
+    <form action="./scripts/4_2_script.php" method="post">
+      <input type="text" name="name" placeholder="Podaj imię"><br><br>
+      <input type="radio" name="figure" value="kwadrat" checked>Kwadrat<br>
+      <input type="radio" name="figure" value="prostokąt">Prostokąt<br>
+      <br>
+      <input type="submit" value="zatwierdz figure">
+    </form>
+  </body>
+</html>
